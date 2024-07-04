6ac318e7cc86323bcc51d15aefd24e267b26eea1
= <=>
2024-07-04 15:05:27 +0530

    Deploy files brought to /(root)

--- netflix-ui/src/components/SignUpBar/SignUpBar.jsx
+++ netflix-ui/src/components/SignUpBar/SignUpBar.jsx
@@ -13,6 +13,25 @@ const SignUpBar = () => {
             Ready to watch? Enter your email to create or restart your
             membership.
           </h3>
+          <div
+            className="signup-form-main"
+            data-hasmessage="false"
+            data-issplitform="false"
+          >
+            <div className="signup-input-block">
+              <label className="signup-label">Email address</label>
+              <div className="signup-input-container">
+                <input
+                  className="signup-input"
+                  autoComplete="email"
+                  type="email"
+                  minLength="5"
+                  maxLength="50"
+                />
+              </div>
+            </div>
+            <button className="signup-input-btn">Get Started</button>
+          </div>
         </form>
       </div>
     </>
